182d7bbf540a61cf3216f3b9de75def97fc6f1e0
BenBMDBA <46876990+BenBMDBA@users.noreply.github.com>
2022-03-01 09:51:46 +1100

    Update controller.py

--- ckanext/cloudstorage/controller.py
+++ ckanext/cloudstorage/controller.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import os.path
 
-from pylons import c
-from pylons.i18n import _
+#from pylons import c
+#from pylons.i18n import _
 
 from ckan import logic, model
 from ckan.lib import base, uploader
